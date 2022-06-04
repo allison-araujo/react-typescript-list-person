4f1b16d040f35ca4031f84a8c9867132bd3466d5
allisonvinicius <allisonvinicius63@gmail.com>
2022-06-04 01:44:32 -0400

    add styling component

--- src/Pages/ProfilePerson/styles.ts
+++ src/Pages/ProfilePerson/styles.ts
@@ -8,3 +8,33 @@ export const Container = styled.div`
   width: 100%;
   color: #fff;
 `;
+
+export const Map = styled.div`
+  display: flex;
+  width: 100%;
+  height: 50vh;
+`;
+
+export const Image = styled.img`
+  width: 100px;
+  align-items: center;
+  height: 100px;
+  border-radius: 100px;
+  justify-content: center;
+`;
+
+export const Name = styled.label`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  font-size: 15px;
+  height: 30px;
+  align-items: center;
+`;
+
+export const Location = styled.span`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
