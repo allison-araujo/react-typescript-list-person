6e8b94f1f557b0016fca50c61ebe1cd9c4b45aa2
allisonvinicius <allisonvinicius63@gmail.com>
2022-06-04 00:46:43 -0400

    values add

---
+++ src/utils/valuesOptions.js
@@ -0,0 +1,8 @@
+const options = [
+  { value: "All", label: "All" },
+  { value: "Nome", label: "nome" },
+  { value: "Genero", label: "genero" },
+  { value: "Idade", label: "Idade" },
+  { value: "Nacionalidade", label: "Nacionalidade" },
+];
+export default options;
