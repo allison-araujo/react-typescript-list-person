accb42605f251c259b9e660fb1c6577115c9adc7
allisonvinicius <allisonvinicius63@gmail.com>
2022-06-05 11:11:45 -0400

    refactory component load

--- src/Components/Loading/styles.ts
+++ src/Components/Loading/styles.ts
@@ -3,10 +3,11 @@ import styled, { keyframes } from "styled-components";
 export const LoadContainer = styled.div`
   display: flex;
   justify-content: center;
+  height: 50px;
   padding: 1rem;
 `;
 
-const Animaiiton = keyframes`
+const Animation = keyframes`
   0%,100%{
   transform:scale(0.5);
   background-color: #d7dbdd;
@@ -25,16 +26,7 @@ export const Spiner = styled.div`
   background-color: #d7dbdd;
   display: inline-block;
   margin: 0.2rem;
-  animation-name: ${Animaiiton};
+  animation-name: ${Animation};
   animation-duration: 20s;
   animation-iteration-count: infinite;
-`;
-
-export const Load = styled.div`
-  border: 16px solid #dcdcdc;
-  border-top: 16px solid rgb(136, 136, 136);
-  border-radius: 50%;
-  width: 40px;
-  height: 40px;
-  animation: spin 2s linear infinite;
 `;
