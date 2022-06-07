8b7ee0a0520efc6a2b5c0849706df517d86c8b11
allisonvinicius <allisonvinicius63@gmail.com>
2022-06-07 00:23:34 -0400

    rm cometary

--- src/Components/ComponentMaps/styles.ts
+++ src/Components/ComponentMaps/styles.ts
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100%;
-  /* background-color: #000; */
 `;
 
 export const ContentMaps = styled.div`
